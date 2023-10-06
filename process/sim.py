00bc62326b84d3da9b3902b22b9aae0b18ad2d7f
inchoxd <rjb2066@gmail.com>
2023-10-06 12:08:22 +0900

    refactored a part of code, created a new func about drawing mpa

--- process/sim.py
+++ process/sim.py
@@ -27,7 +27,7 @@ class Sim:
                     self.goal:tuple = (x, y)
             
 
-    def draw_map(self, ax=None, route:deque=None) -> None:
+    def _draw_map(self, ax=None, route:deque=None) -> None:
         if ax is None:
             fig, ax = plt.subplots(1, 1, figsize=(6, 6))
 
@@ -53,86 +53,6 @@ class Sim:
         tx_go = ax.text(self.goal[0],  self.goal[1],  'G', ha='center', va='center', fontsize=15, c='w', weight='bold')
 
 
-    def _is_valid_move(self, map_data:list, x:int, y:int) -> bool:
-        if 0 <= x < self.width and 0 <= y < self.height:
-            return map_data[y][x] != 'x'
-
-        return False
-
-
-    def create_graph(self, map_data:list) -> list:
-        graph:dict = {}
-        moves:list = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        for y in range(self.height):
-            for x in range(self.width):
-                if map_data[y][x] != 'x':
-                    nb = [ ((x + dx, y + dy), math.sqrt(dx ** 2 + dy ** 2)) for dx, dy in moves if self._is_valid_move(map_data, x + dx, y + dy) ]
-                    graph[(x, y)] = nb
-
-        return graph
-
-
-if __name__ == '__main__':
-    """
-    map_data = [
-            'xxxxxxxxxx',
-            'x        x',
-            'x  xxxx  x',
-            'x     x  x',
-            'x S   x Gx',
-            'x     x  x',
-            'xxxxxxxxxx'
-            ]
-    """
-    map_data = [
-            'xxxxxxxxxxx',
-            'x       xGx',
-            'x xxxxx x x',
-            'x     x   x',
-            'x xxx xxxxx',
-            'x x x     x',
-            'x x xxxxx x',
-            'x   x     x',
-            'x xxx xxx x',
-            'xS    x   x',
-            'xxxxxxxxxxx',
-            ]
-    """
-    map_data = [
-            'xxxxxxxxxxxx',
-            'xSx   x    x',
-            'x    xxx x x',
-            'x xx x   x x',
-            'x      xxx x',
-            'x  x xxx   x',
-            'x xx x   xGx',
-            'x      x xxx',
-            'x xx x     x',
-            'x x  xx xx x',
-            'x     x    x',
-            'xxxxxxxxxxxx'
-            ]
-    map_data = [
-            'xxxxxxxxxxxx',
-            'xSx        x',
-            'x    xxxxx x',
-            'x xx x   x x',
-            'x      xxx x',
-            'x  x xxx   x',
-            'x xx x   xGx',
-            'x      x xxx',
-            'x xx x     x',
-            'x x  xx xx x',
-            'x     x    x',
-            'xxxxxxxxxxxx'
-            ]
-    """
-    sim = Sim(map_data)
-    graph = sim.create_graph(map_data)
-    sim.draw_map()
-    [print(k, graph[k]) for k in graph]
-    route = sim.a_star(graph)
-    print(route)
-    sim.draw_map(route=route)
-
-    plt.show()
+    def show_graph(self) -> None:
+        self._draw_map()
+        plt.show()
