8c04392f7def7c4dcd7f90c9cfe357e29f09e4ef
inchoxd <rjb2066@gmail.com>
2024-08-29 16:43:21 +0900

    added a new option of command

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import sys
 import argparse as argp
 from process import Sim, Graph, Astar
 
@@ -6,6 +7,8 @@ class App:
     def __init__(self):
         prsr = argp.ArgumentParser()
         prsr.add_argument('-f', required=True, help='input file path of map data')
+        prsr.add_argument('-s', required=True, help='input pos of start')
+        prsr.add_argument('-g', required=True, help='input pos of goal')
         prsr.add_argument('--no_route', action='store_false', help='do not show route on the sim')
         prsr.add_argument('--no_anime', action='store_false', help='disable animation')
         prsr.add_argument('--no_plus_score', action='store_false', help='disable new feature')
@@ -13,16 +16,22 @@ class App:
 
         dta:str = ''
         ext:str = self.args.f.split('.')[1]
+        start:tuple = tuple([ int(p) for p in self.args.s.split(',') ])
+        goal:tuple = tuple([ int(p) for p in self.args.g.split(',') ])
+
         with open(self.args.f, 'r') as dta:
             f_data:list = dta.readlines()
             if ext == 'txt':
                 self.map_data = [ line.replace('\n', '') for line in f_data ]
-            elif ext == 'pgm':
-                size:tuple = tuple([ int(pos) if '\n' not in pos else int(pos.replace('\n', '')) for pos in f_data[2].split(' ') ])
+            elif ext == 'pgm' and f_data[0].replace('\n', '') == 'P2':
+                size:tuple = tuple([ int(s) for s in f_data[2].replace('\n', '').split(' ') ])
                 pxls:list = f_data[4:]
-                self.map_data:list = [ [ int(pxl.replace('\n', '')) for line in [ pxls for i in range(size[0] * size[1]) ] for pxl in line ][17*i:17*(i+1)] for i in range(size[1]) ]
+                self.map_data:list = [ [ int(pxl.replace('\n', '')) for line in [ pxls for i in range(size[0] * size[1]) ] for pxl in line ][size[0]*i:size[0]*(i+1)] for i in range(size[1]) ]
+            else:
+                print('\033[31m[ERROR]\033[0m INVALID FILE TYPE!')
+                sys.exit(1)
 
-        self.sim:process.sim.Sim = Sim(self.map_data)
+        self.sim:process.sim.Sim = Sim(self.map_data, start, goal)
         self.graph:process.graph.Graph = Graph(self.map_data)
         self.a_star = Astar()
 
@@ -30,7 +39,7 @@ class App:
     def main(self):
         graph:dict = self.graph.create_graph(self.args.no_plus_score)
         route:list = self.a_star.a_star(graph, self.sim.start, self.sim.goal)
-        self.sim.show_graph(route=route, show_route=(route if self.args.no_route else []), animation=self.args.no_anime)
+        self.sim.show_graph(route=route, show_route=self.args.no_route, animation=self.args.no_anime, graph=(graph if self.args.no_anime and self.args.no_plus_score else self.graph.create_graph(not self.args.no_plus_score) if self.args.no_anime and not self.args.no_plus_score else {}), mode=self.args.no_plus_score)
 
 
 if __name__ == '__main__':
